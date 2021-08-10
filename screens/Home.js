12810d5a81329abf3d883f5d0985cd437e6f3b7f
Spenser Wellen <spenserwellen@gmail.com>
2021-08-10 13:54:37 -0500

    completed header layout and home's scroll-view layout

--- screens/Home.js
+++ screens/Home.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet
+	View,
+	Text,
+	TouchableOpacity,
+	ScrollView,
+	StyleSheet,
 } from 'react-native';
+import { HeaderBar } from '../components';
+
+import {
+	constants,
+	icons,
+	COLORS,
+	FONTS,
+	IMAGES,
+	SIZES,
+	dummyData,
+} from '../constants';
 
 const Home = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <Text>Home</Text>
+	return (
+		<View style={styles.container}>
+			<HeaderBar />
 
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Rewards")}
-            >
-                <Text>Navigate to Rewards</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Location")}
-            >
-                <Text>Navigate to Locations</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+			<ScrollView
+				style={{
+					flex: 1,
+					marginTop: -25,
+					borderTopLeftRadius: SIZES.radius * 2,
+					borderTopRightRadius: SIZES.radius * 2,
+					backgroundColor: COLORS.secondary,
+				}}></ScrollView>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+	container: {
+		flex: 1,
+	},
+});
 
 export default Home;
